2627a15c2c11114ec10c8a6efa2e6348358410f2
Christian Weyer <christian.weyer@thinktecture.com>
2014-05-27 22:31:29 +0200

    Some cleanup

--- articles/articlesRepository.js
+++ articles/articlesRepository.js
@@ -1,25 +1,18 @@
 var edge = require("edge"),
     q = require("q");
 
-module.exports = function(){
+module.exports = function () {
     var connectionString = "server=.;database=productsdemo;integrated security=true";
 
-    var listArticlesConfig = {
-        connectionString: connectionString,
-        source: "select top 10 id, name from articles"
-    };
-
-    var getArticleDetailsConfig = {
-        connectionString: connectionString,
-        source: "select id, name, code, description, imageUrl from articles where id = convert(uniqueidentifier, @id)"
-    };
-
     return{
-        listArticles: function(){
+        listArticles: function () {
             var deferred = q.defer();
 
-            edge.func("sql", listArticlesConfig)(null, function(error, result){
-                if(error) {
+            edge.func("sql", {
+                connectionString: connectionString,
+                source: "select top 10 id, name from articles"
+            })(null, function (error, result) {
+                if (error) {
                     deferred.reject(error);
                 } else {
                     deferred.resolve(result);
@@ -28,11 +21,14 @@ module.exports = function(){
 
             return deferred.promise;
         },
-        getArticleDetails: function(id){
+        getArticleDetails: function (id) {
             var deferred = q.defer();
 
-            edge.func("sql", getArticleDetailsConfig)({ id: id }, function(error, result){
-                if(error) {
+            edge.func("sql", {
+                connectionString: connectionString,
+                source: "select id, name, code, description, imageUrl from articles where id = convert(uniqueidentifier, @id)"
+            })({ id: id }, function (error, result) {
+                if (error) {
                     deferred.reject(error);
                 } else {
                     deferred.resolve(result[0]);

ddaf2263b8724d2bf8fa2e519528fb91d02d3148
Christian Weyer <christian.weyer@thinktecture.com>
2014-05-27 13:26:45 +0200

    SSL

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var express = require("express"),
+var fs = require("fs"),
+    https = require("https"),
+    express = require("express"),
     cors = require("cors"),
     oAuth2Server = require("./security/oAuth2Server");
 
@@ -9,4 +11,8 @@ server.use(oAuth2Server);
 var articles = require("./articles/articlesApi");
 server.use("/api/articles", articles);
 
-server.listen(3000);
+var sslOptions = {
+    key: fs.readFileSync("./setup/key.pem"),
+    cert: fs.readFileSync("./setup/cert.pem")
+};
+https.createServer(sslOptions, server).listen(3000);
